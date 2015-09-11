6b2c832d9374cc4a3dfcf0f0a8082c4790dad411
Aileen Cacayorin <acacayorin@gmail.com>
2015-09-11 12:22:11 -0700

    no longer working

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,31 @@
 var replaceWord = function(string, wordToReplace, newWord) {
   var stringArray = string.toLowerCase().replace(/[.,-\/#!?$%\^&\*;:{}=\-_`~()]/g,"").split(" ");
   for (var i = 0; i < stringArray.length; i++) {
-    if (stringArray[-1] === wordToReplace) {
-      alert(wordToReplace + 'is not in the sentence!');
-    } else {
-      if (stringArray[i] === wordToReplace) {
+    if (stringArray[i] === wordToReplace) {
       stringArray[i] = newWord;
-      };
     };
-  };
     string = stringArray.join(' ');
     return string.charAt(0).toUpperCase() + string.substring(1);
+  };
 };
 
+// var containsWord = function contains(string, wordToReplace) {
+//   var stringArray = string.toLowerCase().replace(/[.,-\/#!?$%\^&\*;:{}=\-_`~()]/g,"").split(" ");
+//   for (var i = 0; i < stringArray.length; i++) {
+//     if (stringArray[i] === wordToReplace) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// };
+//
 $(document).ready(function() {
   $('form#word-sub').submit(function(event){
     var string = ($('input#string').val());
     var wordToReplace = ($('input#word-to-replace').val());
     var newWord = ($('input#new-word').val());
+
     var result = replaceWord(string, wordToReplace, newWord);
 
     $('#result').text(result);
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,4 @@
-describe('repaceWord', function() {
+describe('replaceWord', function() {
   it('will find a word in a string of words', function(){
     var string = 'The night is dark and full of terrors.';
     var wordToReplace = 'dark';
@@ -6,3 +6,11 @@ describe('repaceWord', function() {
     expect(replaceWord(string, wordToReplace, newWord)).to.equal('The night is cold and full of terrors');
   });
 });
+
+// describe('containsWord', function() {
+//   it('will find a word in a string of words', function(){
+//     var string = 'The night is dark and full of terrors.';
+//     var wordToReplace = 'dark';
+//     expect(containsWord(string, wordToReplace).to.equal(true);
+//   });
+// });
