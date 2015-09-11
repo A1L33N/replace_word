ff54dad62c673b5543b9c15efbd9df5913467eaa
Aileen Cacayorin <acacayorin@gmail.com>
2015-09-11 10:37:17 -0700

    function passing specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 var replaceWord = function(string, wordToReplace, newWord) {
   var stringArray = string.toLowerCase().replace(/[.,-\/#!?$%\^&\*;:{}=\-_`~()]/g,"").split(" ");
-  for (var i = 0; i = stringArray.length; i++) {
+  for (var i = 0; i < stringArray.length; i++) {
     if (stringArray[i] === wordToReplace) {
-      stringArray[i] = newWord
+      stringArray[i] = newWord;
     };
   };
+    // (stringArray[0].charAt(0)).toUpperCase();
+     string = stringArray.join(' ');
+     return string.charAt(0).toUpperCase() + string.substring(1);
 };
